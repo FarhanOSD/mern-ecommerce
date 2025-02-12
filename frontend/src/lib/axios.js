497f4ca08fa4093a73f24e1f7e752ac558028341
FarhanOSD <ff3841362@gmail.com>
2025-02-12 20:50:31 +0600

    u

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-	withCredentials: true, // send cookies to the server
+  baseURL: 'https://mensshop.onrender.com/api', // Always use the deployed URL
+  withCredentials: true, // Send cookies to the server
 });
 
 export default axiosInstance;
